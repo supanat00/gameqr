253ebb0e92bad06100bfea6710f63c27db8d6e8b
supanat00 <supanat90@gmail.com>
2023-12-20 13:22:48 +0700

    commit-tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,34 @@
-import { Component } from "react";
-import "./App.css";
+import './App.css'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFullscreen: true,
-    };
-  }
+function App() {
 
-  componentDidMount() {
-    // ตั้งค่าโหมด fullscreen อัติโนมัติ
-    document.documentElement.requestFullscreen();
-  }
+  return (
+    <>
+      
 
-  handleExitClick = () => {
-    // ออกจากโหมด fullscreen เมื่อคลิกปุ่ม
-    document.exitFullscreen();
-  
-    // ปิดเว็บแอพบนมือถือ
-    const { userAgent } = window.navigator;
-    if (userAgent.match(/(iPhone|iPod|iPad|Android)/i)) {
-      window.location.href = "about:blank";
-    } else {
-      window.close();
-    }
-  };
-  
-  
+<div className="mb-4 border-b border-gray-200 dark:border-gray-700">
+    <ul className="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-tab" data-tabs-toggle="#default-tab-content" role="tablist">
+        <li className="me-2" role="presentation">
+            <button className="inline-block p-4 border-b-2 rounded-t-lg" id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Single</button>
+        </li>
+        <li className="me-2" role="presentation">
+            <button className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Group</button>
+        </li>
+    </ul>
+</div>
+<div id="default-tab-content">
+    <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+        <p className="text-sm text-gray-500 dark:text-gray-400"><strong className="font-medium text-gray-800 dark:text-white">Single Choose</strong></p>
+    </div>
 
-  render() {
-    const { isFullscreen } = this.state;
-    return (
-      <div className="App" style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        zIndex: 100,
-      }}>
-        {isFullscreen && (
-          <button className="btn-exit"
-            onClick={this.handleExitClick}
-            style={{
-              position: "fixed",
-              top: '20px',
-              left: '20px',
-              zIndex: 101,
-              background: "rgba(255, 0, 0, 0.3)",
-              borderRadius: '50%',
-              padding: '10px , 20px',
-              cursor: 'pointer',
-            }}
-          >
-            Exit
-          </button>
-        )}
-      </div>
-    );
-  }
+    <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+        <p className="text-sm text-gray-500 dark:text-gray-400"><strong className="font-medium text-gray-800 dark:text-white">Group Choose</strong></p>
+    </div>
+    
+</div>
+
+    </>
+  )
 }
 
-export default App;
+export default App
